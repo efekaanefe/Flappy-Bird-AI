6bf577a4189afa451e4c9ed3319bd0e9f99927b6
efekaanefe <efekaanefe1905@gmail.com>
2023-07-06 17:00:01 +0300

    dummy files dumped

--- train_ai.py
+++ train_ai.py
@@ -3,6 +3,6 @@ from data_init import DataInitializerFB
 import numpy as np
 
 
-nn = MyNeuralNetwork(dataInitializer=DataInitializerFB(), input=7 ,hidden = 5, output=1)
+nn = MyNeuralNetwork(dataInitializer=DataInitializerFB(), input=7 ,hidden = 50, output=1)
 nn.gradient_descent(epochs=10, learning_rate=0.45, batch_size=1854, print_acc=False, plot_acc=False)
 nn.test_accuracy_with_test_data()
